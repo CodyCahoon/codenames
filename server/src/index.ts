f6eb06176de50daa3a8071de7c46715fa6a39995
Cody Cahoon <codyjcahoon@gmail.com>
2020-01-15 21:50:49 -0500

    Sets up basic web socket server for communication

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,18 @@
 import express from 'express';
+import ws from 'ws';
+import http from 'http';
 
 const app = express();
+const server = http.createServer(app);
+const wss = new ws.Server({ server });
 const port = 8080;
 
-app.listen(port, () => {
+wss.on('connection', (ws: WebSocket) => {
+    ws.onmessage = (ev: MessageEvent) => {
+        ws.send('Message received' + ev.data);
+    };
+});
+
+server.listen(port, () => {
     console.log(`server started at http://localhost:${port}`);
 });
